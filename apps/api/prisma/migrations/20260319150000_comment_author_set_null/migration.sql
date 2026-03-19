a45fbd732db10f4c119df8ba4615b0dcec130cf4
sitao <sitao.wang@mail.utoronto.ca>
2026-03-19 14:20:39 -0400

    Clean up migration

--- apps/api/prisma/migrations/20260319150000_comment_author_set_null/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable: make Comment.authorId nullable and change onDelete to SET NULL
-ALTER TABLE "Comment" ALTER COLUMN "authorId" DROP NOT NULL;
-
--- DropForeignKey
-ALTER TABLE "Comment" DROP CONSTRAINT "Comment_authorId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
