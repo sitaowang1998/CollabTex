d2a4290cf3d81ec1b8cad191e0d7bb8509e3cf2c
sitao <sitao.wang@mail.utoronto.ca>
2026-03-31 12:34:05 -0400

    Address comment

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,18 +3,15 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ErrorBoundary } from "./components/ErrorBoundary";
-import { ToastProvider } from "./components/ui/toaster";
 import { router } from "./router";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
-      <ToastProvider>
-        <AuthProvider>
-          <RouterProvider router={router} />
-        </AuthProvider>
-      </ToastProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ErrorBoundary>
   </StrictMode>,
 );
